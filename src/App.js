5c4341a2f8ed529b8e20de9597808e13fe211999
David Tuan <55757870+18521604@users.noreply.github.com>
2021-03-02 21:15:41 +0700

    fix link Monsters with https

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
     this.handleChange = this.handleChange.bind(this);
   }
   componentDidMount(){
-    fetch('http://jsonplaceholder.typicode.com/users')
+    fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
     .then(users => this.setState({monsters : users}));
   }
